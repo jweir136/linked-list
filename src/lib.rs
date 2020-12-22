9e48f71a4713fb5b3c4401b0b9c988374772c64e
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-22 11:56:25 -0500

    implement Node::new and it's tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,23 @@ pub struct Node<T> {
     data: T,
     next: Option<Box<Node<T>>>
 }
+
+impl<T> Node<T> {
+    pub fn new(data: T, next: Option<Box<Node<T>>>) -> Self {
+        Node {
+            data: data,
+            next: next
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn node_new_test() {
+        let n1 = Node::<u32>::new(0, Option::None);
+        let n2 = Node::<u32>::new(1, Option::Some(Box::new(n1)));
+    }
+}
