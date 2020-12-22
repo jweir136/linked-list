42827b5ee5f0623971c7ac70973b77b3c6528b52
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-22 12:14:22 -0500

    change Node implemention to be stored in a seperate file and mod

---
+++ src/node.rs
@@ -0,0 +1,64 @@
+pub struct Node<T> {
+    data: T,
+    next: Option<Box<Node<T>>>
+}
+
+impl<T> Node<T> {
+    pub fn new(data: T, next: Option<Box<Node<T>>>) -> Self {
+        Node {
+            data: data,
+            next: next
+        }
+    }
+
+    pub fn get_data(&self) -> &T {
+        &self.data
+    }
+
+    pub fn get_next(&self) -> Option<&Box<Node<T>>> {
+        self.next.as_ref()
+    }
+
+    pub fn set_data(&mut self, data: T) {
+        self.data = data;
+    }
+
+    pub fn set_next(&mut self, next: Option<Box<Node<T>>>) {
+        self.next = next;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn node_new_test() {
+        let n1 = Node::<u32>::new(0, Option::None);
+        let _n2 = Node::<u32>::new(1, Option::Some(Box::new(n1)));
+    }
+
+    #[test]
+    fn node_getdata_test() {
+        let n1 = Node::<u32>::new(0 as u32, Option::None);
+        let n2 = Node::<u32>::new(1 as u32, Option::Some(Box::new(n1)));
+
+        assert_eq!(*n2.get_data(), 1);
+    }
+
+    #[test]
+    fn node_setdata_test() {
+        let mut n1 = Node::<u32>::new(0 as u32, Option::None);
+        n1.set_data(1 as u32);
+
+        assert_eq!(*n1.get_data(), 1);
+    }
+
+    #[test]
+    fn node_setnext_test() {
+        let n1 = Node::<u32>::new(0 as u32, Option::None);
+        let mut n2 = Node::<u32>::new(1 as u32, Option::Some(Box::new(n1)));
+
+        n2.set_next(Option::None);
+    }
+}
