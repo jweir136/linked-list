949f22a7d955b64059653850721b4ed33eaabd25
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-22 12:21:55 -0500

    implement LinkedList::new

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,27 @@
 pub mod node;
+
+use crate::node::Node;
+
+pub struct LinkedList<T> {
+    root: Option<Box<Node<T>>>,
+    size: usize
+}
+
+impl<T> LinkedList<T> {
+    pub fn new() -> Self {
+        LinkedList {
+            root: Option::None,
+            size: 0
+        }
+    }
+}
+
+#[cfg(test)] 
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ll_new_test() {
+        let _ll = LinkedList::<u32>::new();
+    }
+}
